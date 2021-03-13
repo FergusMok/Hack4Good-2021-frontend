8cee4b7df2579286fa804bfbacedac84a2a0b5dd
FergusMok <FergusMok1@gmail.com>
2021-03-13 14:33:54 +0800

    Bar Chart completed

--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -1,8 +1,14 @@
 import Doughnut from "./ProfileComponents/Doughnut";
 import React from "react";
+import HorizontalBarChart from "./ProfileComponents/BarChart";
 
 const ProfilePage = () => {
-  return <Doughnut />;
+  return (
+    <>
+      <Doughnut />
+      <HorizontalBarChart />
+    </>
+  );
 };
 
 export default ProfilePage;
