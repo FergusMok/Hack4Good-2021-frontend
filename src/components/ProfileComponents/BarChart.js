59e801892d969306f01fd176011a49f76f2d3d01
FergusMok <FergusMok1@gmail.com>
2021-03-17 03:08:25 +0800

    removed overview button for chart

--- src/components/ProfileComponents/BarChart.js
+++ src/components/ProfileComponents/BarChart.js
@@ -78,14 +78,6 @@ const HorizontalBarChart = (props) => {
       <Divider />
       <HorizontalBar data={data} options={options} />
       <Divider />
-      <Box display="flex" justifyContent="flex-end" p={1}>
-        <Button color="primary" endIcon={<ArrowRightIcon />} size="small" variant="text">
-          Overview
-        </Button>
-        <Button color="primary" endIcon={<ArrowRightIcon />} size="small" variant="text">
-          Overview
-        </Button>
-      </Box>
     </Card>
   );
 };
